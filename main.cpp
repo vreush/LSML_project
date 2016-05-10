0f2f61ca418b4068aab17a6220cca3f841741ac9
vreush <vreush@users.noreply.github.com>
2016-05-10 12:45:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,33 +28,17 @@ std::vector<std::string> split(const std::string &s, char delim) {
 }
 
 
-vector<vector<double>> read_csv(std::ifstream& file) {
-    vector<vector<double>> result;
-    string line;
-    std::cout << "in read_csv\n";
-    while (file >> line) {
-        std::cout << "line read\n";
-        vector<double> row;
-        size_t col = 0;
-        for(std::string el : split(line, ',')) {
-            double number = std::stod(el);
-            row.push_back(number);
-            col += 1;
-        }
-        result.push_back(row);
-    }
-    return result;
-}
-
-
-std::vector<std::unordered_map<char,int>> read_data(std::ifstream& file, size_t lines_count) {
-    std::vector<std::unordered_map<char,int>> result;
-    return result;
-}
-
-
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
+}
+
+
+double sign(double x) {
+    if (x >= 0) {
+        return 1;
+    } else {
+        return -1;
+    }
 }
 
 
@@ -62,12 +46,18 @@ double predict_sample_proba(const std::unordered_map<std::string,double>& sample
                const std::unordered_map<std::string,double>& weights) {
     double pred = 0;
     for (auto kv : sample) {
-        pred += weights.find(kv.first)->second * kv.second;
+        auto found = weights.find(kv.first);
+        double coef = 0;
+        if (found != weights.end()) {
+            coef = found->second;
+        }
+        pred += coef * kv.second;
     }
     return sigmoid(pred);
 }
 
-std::vector<double> predict_sample_proba(const std::vector<std::unordered_map<std::string, double>>& samples,
+
+std::vector<double> predict_samples_proba(const std::vector<std::unordered_map<std::string, double>>& samples,
                             const std::unordered_map<std::string,double>& weights) {
     std::vector<double> result;
     result.reserve(samples.size());
@@ -77,93 +67,117 @@ std::vector<double> predict_sample_proba(const std::vector<std::unordered_map<st
     return result;
 }
 
-const std::unordered_map<std::string,double> grad(const std::unordered_map<std::string,double>& sample,
-                                                  double ground_truth,
-                                                  const std::unordered_map<std::string,double>& weights) {
-    std::unordered_map<std::string,double> result;
-    double mult = (predict_sample_proba(sample, weights) - ground_truth);
-    for (auto kv : sample) {
-         result[kv.first] = mult * kv.second;
-    }
-    return result;
-}
-
-void print_data(const std::unordered_map<std::string,double>& data) {
-    for (auto kv: data) {
-        std::cout << kv.first << ": " << kv.second << "; ";
-    }
-    std::cout << "\n";
-}
 
 double log_loss(const std::vector<double>& y_pred, const std::vector<int>& y_true) {
     double loss = 0;
     for (size_t index = 0; index < y_pred.size(); ++index) {
         loss += y_true[index] * log(y_pred[index]) + ((1.0 - y_true[index]) * (log(1.0 - y_pred[index])));
     }
-    return -loss;
+    return -loss / y_pred.size();
 }
 
-void update(std::unordered_map<std::string,double >& weights,
-            const std::unordered_map<std::string,double>& grad,
-            double learning_rate, double lambda) {
-    for (auto kv : grad) {
-        weights[kv.first] -= ((learning_rate * grad.find(kv.first)->second) + (lambda * weights[kv.first]));
+
+// ftrl
+struct Params {
+    double alpha;
+    double beta;
+    double lambda1;
+    double lambda2;
+    std::unordered_map<std::string,double> z; 
+    std::unordered_map<std::string,double> n;
+    std::unordered_map<std::string,double> g;
+    std::unordered_map<std::string,double> weights;
+    std::unordered_map<std::string,double> sigma;
+};
+
+
+std::unordered_map<std::string,double> run_ftrl(const std::vector<std::unordered_map<std::string,double>>& samples,
+               const std::vector<int> labels,
+               Params &params) {
+
+    for (size_t index = 0; index < samples.size(); ++index) {
+        const auto& sample = samples[index];
+        int label = labels[index];
+        for(const auto& item: sample) {
+             auto feature = item.first;
+             auto val = item.second;
+             params.weights[feature] = 0;
+             auto eta = (1.0 / (
+                     (params.beta + sqrt(params.n[feature]) / params.alpha
+                         )
+                     + params.lambda2));
+             if (params.z[feature] >= params.lambda1) {
+                 params.weights[feature] = - eta * (params.z[feature] - (sign(params.z[feature]) * params.lambda1));
+             }
+
+             double pt = predict_sample_proba(sample, params.weights);
+
+             params.g[feature] = (pt - label) * sample.at(feature);
+             params.sigma[feature] = (sqrt(params.n[feature] + (params.g[feature] * params.g[feature])) - sqrt(params.n[feature])) / params.alpha;
+             params.z[feature] = params.z[feature] + params.g[feature] - (params.sigma[feature] * params.weights[feature]);
+             params.n[feature] = params.n[feature] + (params.g[feature] * params.g[feature]);
+        }
     }
-}
-
-void do_sgd_step(const std::unordered_map<std::string,double>& sample,
-                 double ground_truth,
-                 std::unordered_map<std::string,double>& weights,
-                 double learning_rate,
-                 double lambda) {
-    auto cur_grad = grad(sample, ground_truth, weights);
-    update(weights, cur_grad, learning_rate, lambda);
+    return params.weights;
 }
 
 
-size_t get_rand_index(size_t max_index) {
-    std::default_random_engine generator;
-    std::uniform_int_distribution<size_t> distribution(0, max_index);
-    return distribution(generator);
-}
+std::unordered_map<std::string,double> batch_train_ftrl(std::ifstream& file, size_t batch_size,
+    double alpha, double beta, double lambda1, double lambda2) {
 
-std::unordered_map<std::string,double> train(const std::vector<std::unordered_map<std::string,double>>& samples,
-           const std::vector<int> ground_truth, double learning_rate, double lambda, unsigned int max_iter) {
-    std::cout << "in train\n";
-    std::cout << samples.size() << "\n";
-    std::unordered_map<std::string,double> weights = samples[0];
-    // std::unordered_map<std::string,double> weights = std::unordered_map<std::string,double>(samples[0]);
-    std::cout << "in train2\n";
-    for (auto kv: weights) {
-        std::cout << kv.first << "\n";
-        weights[kv.first] = 1.0;
+    std::vector<std::unordered_map<std::string,double>> features;
+    vector<int> ground_truth;
+
+    features.reserve(batch_size);
+    ground_truth.reserve(batch_size);
+
+    string line;
+    size_t count = 0;
+    Params params;
+    params.alpha = alpha;
+    params.beta = beta;
+    params.lambda1 = lambda1;
+    params.lambda2 = lambda2;
+
+    std::unordered_map<std::string,double> weights;
+    while (file >> line) {
+        count += 1;
+        std::unordered_map<std::string,double> row;
+        bool is_first = true;
+        for(std::string el : split(line, ',')) {
+            if (!is_first) {
+                auto name_val = split(el, ':');
+                double number = std::stod(name_val[1]);
+                row[name_val[0]] = number;
+            } else {
+                ground_truth.push_back(std::stoi(el));
+                is_first = false;
+            }
+        }
+        features.push_back(row);
+        if ((count % 10000) == 0) {
+            std::cout << "count = " << count << "\n";            
+        }
+        if (count >= batch_size) {
+            std::cout << "count = " << count << "\n";
+            weights = run_ftrl(features, ground_truth, params);
+            
+            count = 0;
+            
+            features.clear();
+            ground_truth.clear();
+            
+            features.reserve(batch_size);
+            ground_truth.reserve(batch_size);
+        }
     }
-    std::cout << "init weights\n";
-    print_data(weights);
-    for (size_t i = 0; i < max_iter; ++i) {
-        size_t index = get_rand_index(samples.size());
-        auto sample = samples[index];
-        double true_label = ground_truth[index];
-        do_sgd_step(sample, true_label, weights, learning_rate, lambda);
-
-        i += 1;
-        std::cout << "after iter " << i << ":\n";
-        print_data(weights);
-        std::cout << "-----log loss: " << log_loss(predict_sample_proba(samples, weights), ground_truth) << "\n";
+    if (count > 0) {
+            weights = run_ftrl(features, ground_truth, params);
     }
+    std::cout << "training done--------------------------------------------------\n";
     return weights;
 }
 
-
-std::vector<double> predict_proba(const std::unordered_map<std::string,double>& weights,
-                                  const std::vector<std::unordered_map<std::string,double>>& samples) {
-    std::vector<double> probas;
-    probas.reserve(samples.size());
-    for (const auto& sample: samples) {
-        probas.push_back(predict_sample_proba(sample, weights));
-    }
-    return probas;
-}
 
 void export_data(const std::vector<double>& preds, std::ofstream& out) {
     for (const auto& pred: preds) {
@@ -172,97 +186,87 @@ void export_data(const std::vector<double>& preds, std::ofstream& out) {
 }
 
 
-struct Data {
+void batch_predict_ftrl(std::ifstream& file, std::ofstream& out, size_t batch_size,
+    const std::unordered_map<std::string,double>& weights) {
     std::vector<std::unordered_map<std::string,double>> features;
-    std::vector<int> ground_truth;
-};
+    vector<int> ground_truth;
+    vector<double> preds;
 
+    features.reserve(batch_size);
+    ground_truth.reserve(batch_size);
 
-Data read_data(std::ifstream& input) {
-    auto data = read_csv(input);
-
-    std::vector<std::unordered_map<std::string,double>> data_tr;
-    std::vector<int> ground_truth;
-
-    for (const auto& row: data) {
-        std::unordered_map<std::string,double> row_tr;
-        ground_truth.push_back(row[0]);
-        for (size_t i = 1; i < row.size(); ++i) {
-            row_tr[std::to_string(i)] = row[i];
+    string line;
+    size_t count = 0;
+    Params params;
+    while (file >> line) {
+        count += 1;
+        std::unordered_map<std::string,double> row;
+        bool is_first = true;
+        for(std::string el : split(line, ',')) {
+            if (!is_first) {
+                auto name_val = split(el, ':');
+                double number = std::stod(name_val[1]);
+                row[name_val[0]] = number;
+            } else {
+                ground_truth.push_back(std::stoi(el));
+                is_first = false;
+            }
         }
-        data_tr.push_back(row_tr);
-        std::cout << "\n";
+        features.push_back(row);
+        if ((count % 10000) == 0) {
+            std::cout << "count = " << count << "\n";            
+        }
+        if (count >= batch_size) {
+            vector<double> batch_preds = predict_samples_proba(features, weights);
+            preds.insert(preds.end(), batch_preds.begin(), batch_preds.end());
+            
+            count = 0;
+            
+            features.clear();            
+            features.reserve(batch_size);
+        }
     }
-    std::cout << "data len:" << data_tr.size() << "\n";
-
-//    for (const auto& row: data_tr) {
-//        for (auto kv: row) {
-//            std::cout << kv.first << " ";
-//            std::cout << kv.second << " ";
-//        }
-//        std::cout << "\n";
-//    }
-    return Data{data_tr, ground_truth};
+    if (count > 0) {
+        vector<double> batch_preds = predict_samples_proba(features, weights);
+        preds.insert(preds.end(), batch_preds.begin(), batch_preds.end());
+    }
+    std::cout << "----- test log loss: " << log_loss(preds, ground_truth) << "\n";
+    std::cout << "--------------------------------------------------\n";
+    export_data(preds, out);
 }
+
 
 int main(int argc, char* argv[]) {
     std::string filename_train = argv[1];
     std::string filename_test = argv[2];
     std::string filename_out = argv[3];
-    // std::string filename_train = "train.csv";
-    // std::string filename_test = "train.csv";
-    // std::string filename_out = "out.csv";
+    size_t batch_size = std::stoi(argv[4]);
+    double alpha = std::stod(argv[5]);
+    double beta = std::stod(argv[6]);
+    double lambda1 = std::stod(argv[7]);
+    double lambda2 = std::stod(argv[8]);
 
-    std::cout << filename_train << "\n";
-    std::cout << filename_test << "\n";
-    std::cout << filename_out << "\n";
-    std::cout << "cin\n";
-
-    auto dir = std::string("");
-
-//    std::cin >> filename_test;
-    filename_train = dir + filename_train;
-    filename_test = dir + filename_test;
-    filename_out = dir + filename_out;
-
-    std::cout << "train path: " << filename_train << "\n";
+    std::cout << "train: " << filename_train << "\n";
+    std::cout << "test: " << filename_test << "\n";
+    std::cout << "out: " << filename_out << "\n";
+    std::cout << "batch_size: " << batch_size << "\n";
+    std::cout << "alpha: " << alpha << "\n";
+    std::cout << "beta: " << beta << "\n";
+    std::cout << "lambda1: " << lambda1 << "\n";
+    std::cout << "lambda2: " << lambda2 << "\n";
 
     ifstream input_train;
     input_train.open(filename_train);
-    auto train_data = read_data(input_train);
+    std::unordered_map<std::string,double> model_weights = batch_train_ftrl(input_train, batch_size, alpha, beta, lambda1, lambda2);
     input_train.close();
-    if (train_data.features.size() == 0) {
-        throw std::runtime_error("No train file at " + filename_train);
-    }
-    
-
 
     ifstream input_test;
     input_test.open(filename_test);
-    auto test_data = read_data(input_test);
-    input_test.close();
-    if (test_data.features.size() == 0) {
-        throw std::runtime_error("No test file at " + filename_test);
-    }
-
-
-
-    double learning_rate = 0.1;
-    double lambda = 0;
-    unsigned int max_iter = 1000;
-
-    std::cout << "train\n";
-    train_data.features;
-    train_data.ground_truth;
-    auto model_weights = train(train_data.features, train_data.ground_truth, learning_rate, lambda, max_iter);
-    std::cout << "train end\n";
-
-    auto test_preds = predict_proba(model_weights, test_data.features);
-
     ofstream out;
     out.open(filename_out);
-    export_data(test_preds, out);
-    out.close();
+
+    batch_predict_ftrl(input_test, out, batch_size, model_weights);
+    input_test.close();
 
     return 0;
 }
